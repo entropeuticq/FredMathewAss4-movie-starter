62f37b45404c2d8e9dd793ff449a8d4d397a7558
Entropeutic <freds333@uw.edu>
2024-05-09 21:31:21 +0000

    updated customer.h, inventory.h, movie.h, transaction.h

--- Inventory.h
+++ Inventory.h
@@ -3,22 +3,31 @@
 
 #include "Movie.h"
 #include <vector>
-#include <map>
+#include <unordered_map>
 #include <string>
 
 
 class Inventory {
 public:
-    //adds a movie to inventory
+    //adds a movie to inventory based on its type
     void addMovie(Movie* movie);
 
     //finds a movie in inventory by type and attributes
+    //firstAttribute is primary sorting attribute
+    //secondAttribute is secondary sorting attribute
     Movie* findMovie(char type, const string& firstAttribute, const string& secondAttribute);
 
-    //display movies in inventory
+    //display movies in inventory catagorized by movie type
+    //each catagory will be sorted based on required sorting attributes
     void display() const;
 
 private:
+    //maps for quick searches using unique keys
+    unordered_map<string, Movie*> comedyMap;
+    unordered_map<string, Movie*> dramaMap;
+    unordered_map<string, Movie*> classicMap;
+
+    //vectors that will keep sorted collections
     vector<Movie*> comedies;
     vector<Movie*> dramas;
     vector<Movie*> classics;
@@ -31,6 +40,18 @@ private:
 
     //sorts classics by release date and then major actor
     void sortClassics();
+
+    //for making unique keys for comdies based on title and year
+    string buildComedyKey(const string& title, int year);
+
+    //for making unique keys for dramas based on director and title
+    string buildDramaKey(const string& director, const string& title);
+
+    //for making unique keys for classic movies bsed on release date and major actor
+    string buildClassicKey(int month, int year, const string& majorActor);
+
+    
 };
 
 #endif
+
