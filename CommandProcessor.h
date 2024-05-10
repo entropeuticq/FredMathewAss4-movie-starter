f6c881892b699b673022cd57dbddcc8a59263e51
Entropeutic <freds333@uw.edu>
2024-05-10 00:03:14 +0000

    created CustomerManager and CommandProcessor class and declared functions and datat members

---
+++ CommandProcessor.h
@@ -0,0 +1,45 @@
+#ifndef COMMANDPROCESSOR_H
+#define COMMANDPROCESSOR_H
+
+#include "Inventory.h"
+#include "CustomerManager.h"
+#include "Transaction.h"
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+using namespace std;
+
+//class for handling reading and processing commands from a file
+//uses Inventory and CustomerManager classes for this purpose
+class CommandProcessor {
+public:
+    //constructor, initialize with refrecnes to inventory and customer manager
+    CommandProcessor(Inventory& inventory, CustomerManager& customerManager);
+
+    //function to process commands from given files
+    void processCommands(const string& filename);
+
+private:
+    Inventory& inventory;
+    CustomerManager& customerManager;
+
+    //execute borrow command
+    void processBorrowCommand(int customerID, char mediaType, char movieType, const string& attributes);
+    
+    //execute return command
+    void processReturnCommand(int customerID, char mediaType, char movieType, const string& attributes);
+
+    //execute inventory display command
+    void processInventoryCommand();
+
+    //execute transaction history command
+    void processHistoryCommand(int customerID);
+
+    //process command strings
+    void processCommandString(const string& command);
+
+};
+#endif
+
---
+++ CustomerManager.h
@@ -0,0 +1,33 @@
+
+
+#ifndef CUSTOMERMANAGER_H
+#define CUSTOMERMANAGER_H
+
+#include "Customer.h"
+#include <unordered_map>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+class CustomerManager {
+public:
+    //loads customers from given file
+    void loadCustomers(const string& filename);
+
+    //lookup customers using unique ID
+    Customer* findCustomer(int customerID) const;
+
+    //show customers in the system
+    void displayCustomers() const;
+
+private:
+    //maps for quick searches using customer ID
+    unordered_map<int, Customer*> customerMap;
+
+    //add new customer to the system
+    void addCustomer(int customerID, const string& firstName, const string& lastName);
+
+};
+#endif
+
