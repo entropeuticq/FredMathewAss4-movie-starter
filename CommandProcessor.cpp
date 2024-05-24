292c02b2749c14543e57f9ed732d7feec5429dea
Entropeutic <freds333@uw.edu>
2024-05-24 19:49:07 +0000

    started CommandProcessor.cpp

---
+++ CommandProcessor.cpp
@@ -0,0 +1,131 @@
+#include "CommandProcessor.h"
+
+//constructor
+CommandProcessor::CommandProcessor(Inventory& inventory, CustomerManager& customerManager)
+    : inventory(inventory), customerManager(customerManager) {}
+
+//process commands read from file
+void CommandProcessor::processCommands(const string& filename) {
+    //opening file
+    ifstream infile(filename);
+    if (!infile) {
+        cerr << "Error: could not open file " << filename << endl;
+        return;
+    }
+
+    string line;
+    //read file line by line
+    while (getline(infile, line)) {
+        //process lines when theyre not empty
+        if (!line.empty()) {
+            processCommandString(line);
+        }
+    }
+}
+
+//function for processing command strings
+void CommandProcessor::processCommandString(const string& command) {
+    //create a string stream using command string
+    istringstream iss(command);
+
+    //reading command types B, R, I, H
+    char commandType;
+    iss >> commandType;
+
+    //switch based on different command types
+    switch (commandType) {
+        case 'B' : {
+            //borrow command - read ID, media tpye and move type
+            int customerID;
+            char mediaType;
+            char movieType;
+            iss >> customerID >> mediaType >> movieType;
+
+            //read the rest of the line as attributes
+            string attributes;
+            getline(iss, attributes);
+
+            //processing borrow command
+            processBorrowCommand(customerID, mediaType, movieType, attributes);
+            break;
+        }
+        case 'R' : {
+            //return command - ID, mediat type and movie type
+            int customerID;
+            char mediaType;
+            char movieType;
+            iss >> customerID >> mediaType >> movieType;
+
+            //reading rest of line as attributes
+            string attributes;
+            getline(iss, attributes);
+
+            //process return command
+            processReturnCommand(customerID, mediaType, movieType, attributes);
+            break;
+        }
+        case 'I' : {
+            //inventory command - processes inventory display
+            processInventoryCommand();
+            break;
+        }
+        case 'H' : {
+            //history command - reads customer ID
+            int customerID;
+            iss >> customerID;
+
+            //process history command
+            processHistoryCommand(customerID);
+            break;
+        }
+        default:
+            //prints error message for invalid command types
+            cerr << "Error: Invalid command type " << commandType << " in command: " << command << endl;
+            break;
+    }
+}
+
+//processes borrow commands
+void CommandProcessor::processBorrowCommand(int customerID, char mediaType, char movieType, const string& attributes) {
+    //validates media type, only one type currently, D for DVD
+    if (mediaType != 'D') {
+        cerr << "Error: Invalid media type " << mediaType << " for borrow command." << endl;
+        return;
+    }
+
+    //finds customer by ID
+    Customer* customer = customerManager.findCustomer(customerID);
+    if (!customer) {
+        //error if customer not found
+        cerr << "Error: customer ID " << customerID << " not found." << endl;
+        return;
+    }
+
+    //finds movie by type and attributes
+    Movie* movie = inventory.findMovie(movieType, attributes);
+    if (!movie) {
+        //error if movie not found
+        cerr << "Error: Movie not found for borrow command with attributes: " << attributes << endl;
+        return;
+    }
+
+    //creates a borrow transaction and executes it
+    Borrow* borrowTransaction = new Borrow(customer, movie);
+    borrowTransaction->execute();
+}
+
+//processing return commands
+void CommandProcessor::processReturnCommand(int customerID, char mediaType, char movieType, const string& attributes) {
+
+}
+
+//processing inventory display commands
+void CommandProcessor::processInventoryCommand() {
+
+}
+
+//processing history commands
+void CommandProcessor::processHistoryCommand(int customerID) {
+    
+}
+
